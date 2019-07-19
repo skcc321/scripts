7d286763c0916b1b34495617146f04351fa16ae1
rafael <skcc321@gmail.com>
2019-07-19 21:36:03 +0300

    simplify tmux & oh-my-zsh

--- install/tmux.sh
+++ install/tmux.sh
@@ -1,7 +1,4 @@
-sudo apt-get install -y python-software-properties software-properties-common #install dependencies
-sudo add-apt-repository -y ppa:pi-rho/dev #add repository
-sudo apt-get update #update list
-sudo apt-get install -y tmux #install tmux
+sudo apt install -y tmux #install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm #install tpm
 mkdir ~/.tmux/config #create conf dir
 git clone https://github.com/skcc321/tmux.git ~/.tmux/config/ #download config
--- install/zsh.sh
+++ install/zsh.sh
@@ -1,1 +1,2 @@
 sudo apt install zsh -y # install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" # install oh-my-zsh
